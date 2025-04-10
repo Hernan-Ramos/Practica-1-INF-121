c83582931aeb7d2090e4e2473812c09a7e552459
Hernan-Ramos <diegohernanramoschoque@gmail.com>
2025-04-10 14:42:03 -0400

    Delete POLIMORFISMO directory

--- POLIMORFISMO/EJERCICIO-7/src/Pajaro.java
+++
@@ -1,8 +0,0 @@
-public class Pajaro {
-    public String hacerSonido(String sonido){
-        return "el pajaro " + sonido;
-    }
-    public void moverse (){
-        System.out.println("El pajaro esta volando");;
-    }
-}
--- POLIMORFISMO/EJERCICIO-7/src/Perro.java
+++
@@ -1,9 +0,0 @@
-public class Perro {
-    //atributos
-    public String hacerSonido(String sonido){
-        return "el perro  " + sonido ;
-    }
-    public void moverse(){
-        System.out.println("El perro corre");;
-    }
-}
